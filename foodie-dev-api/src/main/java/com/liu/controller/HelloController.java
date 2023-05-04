9f3b1dcf17df69a6bde54ea4d5eb8e6ca2be5228
MisteryLiuCode <misteryliu@outlook.com>
2023-05-04 18:27:04 +0800

    4-14

--- foodie-dev-api/src/main/java/com/liu/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/liu/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.liu.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +16,11 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping(method = {RequestMethod.POST,RequestMethod.GET})
 public class HelloController {
 
-    //final private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    final private static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public Object hello() {
+        logger.info("hello日志");
         return "Hello World~";
     }
 
